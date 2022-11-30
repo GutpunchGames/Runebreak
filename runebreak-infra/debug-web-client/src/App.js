2236b7e1edd8852a97027163fcdb87a73b41c39f
AndyG <anderson.garron@gmail.com>
2022-11-29 20:31:24 -0800

    fixed some warnins

--- runebreak-infra/debug-web-client/src/App.js
+++ runebreak-infra/debug-web-client/src/App.js
@@ -1,7 +1,6 @@
 import RegisterForm from './RegisterForm'
-import logo from './logo.svg';
 import './App.css';
-import React, {Text, useEffect, useState} from 'react';
+import React from 'react';
 import doRegister from './doRegister';
 
 function App() {
--- runebreak-infra/debug-web-client/src/RegisterForm.js
+++ runebreak-infra/debug-web-client/src/RegisterForm.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, {Text, useEffect, useState} from 'react';
+import React from 'react';
 
 class RegisterForm extends React.Component {
   constructor(props) {
