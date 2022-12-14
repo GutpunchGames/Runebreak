d1329b34eb669b149ed870ccc4d84a57458aaf73
AndyG <anderson.garron@gmail.com>
2022-12-14 14:52:53 -0800

    have createUser returning the new user id

--- runebreak-infra/accounts/server/Server.go
+++ runebreak-infra/accounts/server/Server.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/GutpunchGames/Runebreak/runebreak-infra/protos/accounts"
@@ -22,33 +23,33 @@ func NewServer(l hclog.Logger) *AccountsServer {
 
 func (server AccountsServer) Register(ctx context.Context, req *accounts.UserAuthenticationRequest) (*accounts.UserAuthenticationResponse, error) {
 	server.Logger.Info("Handle Register", "username", req.Username, "pw", req.Password)
-	err := server.insertUserIntoDb(req.Username, req.Password)
+	userId, err := server.createUser(req.Username, req.Password)
 	if err == nil {
-		return &accounts.UserAuthenticationResponse{UserId: "1234", Username: req.Username}, nil
+		return &accounts.UserAuthenticationResponse{UserId: strconv.FormatInt(*userId, 10), Username: req.Username}, nil
 	} else {
 		return nil, err
 	}
 }
 
-func (server AccountsServer) insertUserIntoDb(username string, password string) error {
+func (server AccountsServer) createUser(username string, password string) (*int64, error) {
 	fmt.Printf("attempting to interface with db\n")
 	db, err := sql.Open("mysql", "accountsservice:accountsservice_pw@tcp(localhost:3306)/accounts")
     defer db.Close()
 
 	if err != nil {
 		fmt.Printf("bad stuff happened 1: %s\n", err)
-		return err
+		return nil, err
 	}
 
 	doesUserExist, err := server.doesUserExist(username)
 	if err != nil {
 		fmt.Printf("bad stuff happened 2: %s\n", err)
-		return err
+		return nil, err
 	}
 
 	if (doesUserExist != nil && *doesUserExist) {
 		server.Logger.Error("User with username already exists.", "username", username)
-		return errors.New(fmt.Sprintf("User with username %s already exists.", username))
+		return nil, errors.New(fmt.Sprintf("User with username %s already exists.", username))
 	}
 
 	query := "INSERT INTO accounts(user_name, authentication_code) VALUES (?, ?)"
@@ -58,23 +59,32 @@ func (server AccountsServer) insertUserIntoDb(username string, password string) 
     stmt, err := db.PrepareContext(ctx, query)
     if err != nil {
 		server.Logger.Error("Error when preparing SQL statement", "error", err)
-		return err
+		return nil, err
     }
     defer stmt.Close()
 
 	res, err := stmt.ExecContext(ctx, username, password)  
 	if err != nil {  
 		server.Logger.Error("Error when inserting row into accounts table", "error", err)
-		return err
+		return nil, err
 	}
 	rows, err := res.RowsAffected()  
 	if err != nil {  
 		server.Logger.Error("Error when finding rows affected", "error", err)
-		return err
+		return nil, err
 	}
 	server.Logger.Info(fmt.Sprintf("%d users created", rows))
 
-	return nil
+    var newUserId int64
+    query = "SELECT LAST_INSERT_ID()"
+    err = db.QueryRow(query).Scan(&newUserId)
+
+	if (err != nil) {
+		server.Logger.Error("Error accessing new user", "error", err)
+		return nil, err
+	}
+
+	return &newUserId, nil
 }
 
 func (server AccountsServer) doesUserExist(username string) (*bool, error) {
