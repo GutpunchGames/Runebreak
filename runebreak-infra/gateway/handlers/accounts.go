2a2a2e249e7921ea7eedd63f00e7aab0eeb43950
AndyG <anderson.garron@gmail.com>
2022-12-19 12:53:31 -0800

    deleted some pasta

--- runebreak-infra/gateway/handlers/accounts.go
+++ runebreak-infra/gateway/handlers/accounts.go
@@ -40,25 +40,3 @@ func (handler *AccountsHandler) UpdateUser(rw http.ResponseWriter, req *http.Req
 	handler.logger.Printf("got user id '%s' and token '%s'\n", userId, token)
 	rw.Write([]byte("{}"))
 }
-
-// func (handler *AccountsHandler) rpcLogin(username string, password string) (*accounts.UserAuthenticationResponse, error) {
-// 	conn, err := grpc.Dial("accounts:9090", grpc.WithInsecure())
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer conn.Close()
-
-// 	accountsClient := accounts.NewAccountsClient(conn)
-// 	req := accounts.UserAuthenticationRequest{Username: username, Password: password}
-// 	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
-// 	defer cancel()
-
-// 	resp, err := accountsClient.Login(ctx, &req)
-// 	if err == nil {
-// 		handler.logger.Printf("got response: %s\n", resp)
-// 		return resp, nil
-// 	} else {
-// 		handler.logger.Printf("got error: %s\n", err)
-// 		return nil, err
-// 	}
-// }
