0cb5b1e6ff829e7244233e801c82c8a633956d72
AndyG <anderson.garron@gmail.com>
2023-01-10 23:21:47 -0800

    added some boilerplate for joining lobbies

--- runebreak-game/RunebreakGame/Source/RunebreakGame/Private/RunebreakAPI/REST/Lobbies.cpp
+++ runebreak-game/RunebreakGame/Source/RunebreakGame/Private/RunebreakAPI/REST/Lobbies.cpp
@@ -24,3 +24,72 @@ void UFetchLobbiesTransaction::OnResponseReceived(FHttpRequestPtr request, FHttp
 	UE_LOG(LogTemp, Warning, TEXT("got response %s"), *respJson);
 	OnSuccess(*resp);
 }
+
+UGetLobbyTransaction::UGetLobbyTransaction() {
+
+}
+
+void UGetLobbyTransaction::Execute(FString token, FString lobbyId) {
+	FHttpRequestRef Request = FHttpModule::Get().CreateRequest();
+	Request->OnProcessRequestComplete().BindUObject(this, &UGetLobbyTransaction::OnResponseReceived);
+	Request->SetURL("http://localhost:9000/lobbies");
+	Request->SetVerb("GET");
+	Request->SetHeader("Content-Type", "application/json");
+	Request->SetHeader("X-Auth-Token", token);
+	Request->SetContentAsString("{\"lobbyId\":\"" + lobbyId + "\"}");
+	Request->ProcessRequest();
+}
+
+void UGetLobbyTransaction::OnResponseReceived(FHttpRequestPtr request, FHttpResponsePtr Response, bool bConnectedSuccessfully) {
+	FString respJson = *Response->GetContentAsString();
+	FLobby* resp = new FLobby();
+	FromJson(respJson, resp);
+	UE_LOG(LogTemp, Warning, TEXT("got response %s"), *respJson);
+	OnSuccess(*resp);
+}
+
+UJoinLobbyTransaction::UJoinLobbyTransaction() {
+
+}
+
+void UJoinLobbyTransaction::Execute(FString token, FString lobbyId) {
+	FHttpRequestRef Request = FHttpModule::Get().CreateRequest();
+	Request->OnProcessRequestComplete().BindUObject(this, &UJoinLobbyTransaction::OnResponseReceived);
+	Request->SetURL("http://localhost:9000/lobbies/join");
+	Request->SetVerb("PATCH");
+	Request->SetHeader("Content-Type", "application/json");
+	Request->SetHeader("X-Auth-Token", token);
+	Request->SetContentAsString("{\"lobbyId\":\"" + lobbyId + "\"}");
+	Request->ProcessRequest();
+}
+
+void UJoinLobbyTransaction::OnResponseReceived(FHttpRequestPtr request, FHttpResponsePtr Response, bool bConnectedSuccessfully) {
+	FString respJson = *Response->GetContentAsString();
+	FLobby* resp = new FLobby();
+	FromJson(respJson, resp);
+	UE_LOG(LogTemp, Warning, TEXT("got response %s"), *respJson);
+	OnSuccess(*resp);
+}
+
+ULeaveLobbyTransaction::ULeaveLobbyTransaction() {
+
+}
+
+void ULeaveLobbyTransaction::Execute(FString token, FString lobbyId) {
+	FHttpRequestRef Request = FHttpModule::Get().CreateRequest();
+	Request->OnProcessRequestComplete().BindUObject(this, &ULeaveLobbyTransaction::OnResponseReceived);
+	Request->SetURL("http://localhost:9000/lobbies/leave");
+	Request->SetVerb("PATCH");
+	Request->SetHeader("Content-Type", "application/json");
+	Request->SetHeader("X-Auth-Token", token);
+	Request->SetContentAsString("{\"lobbyId\":\"" + lobbyId + "\"}");
+	Request->ProcessRequest();
+}
+
+void ULeaveLobbyTransaction::OnResponseReceived(FHttpRequestPtr request, FHttpResponsePtr Response, bool bConnectedSuccessfully) {
+	FString respJson = *Response->GetContentAsString();
+	FLobby* resp = new FLobby();
+	FromJson(respJson, resp);
+	UE_LOG(LogTemp, Warning, TEXT("got response %s"), *respJson);
+	OnSuccess(*resp);
+}
--- runebreak-game/RunebreakGame/Source/RunebreakGame/Public/RunebreakAPI/REST/Lobbies.h
+++ runebreak-game/RunebreakGame/Source/RunebreakGame/Public/RunebreakAPI/REST/Lobbies.h
@@ -97,3 +97,72 @@ public:
 private:
 	void OnResponseReceived(FHttpRequestPtr request, FHttpResponsePtr Response, bool bConnectedSuccessfully);
 };
+
+UINTERFACE(Blueprintable)
+class ULobbyResultHandler : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class RUNEBREAKGAME_API ILobbyResultHandler
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void OnSuccess(FLobby lobby);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void OnFailure();
+};
+
+UCLASS()
+class RUNEBREAKGAME_API UJoinLobbyTransaction : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	TFunction<void(FLobby)> OnSuccess;
+
+public:
+	UJoinLobbyTransaction();
+
+	void Execute(FString token, FString lobbyId);
+
+private:
+	void OnResponseReceived(FHttpRequestPtr request, FHttpResponsePtr Response, bool bConnectedSuccessfully);
+};
+
+UCLASS()
+class RUNEBREAKGAME_API ULeaveLobbyTransaction : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	TFunction<void(FLobby)> OnSuccess;
+
+public:
+	ULeaveLobbyTransaction();
+
+	void Execute(FString token, FString lobbyId);
+
+private:
+	void OnResponseReceived(FHttpRequestPtr request, FHttpResponsePtr Response, bool bConnectedSuccessfully);
+};
+
+UCLASS()
+class RUNEBREAKGAME_API UGetLobbyTransaction : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	TFunction<void(FLobby)> OnSuccess;
+
+public:
+	UGetLobbyTransaction();
+
+	void Execute(FString token, FString lobbyId);
+
+private:
+	void OnResponseReceived(FHttpRequestPtr request, FHttpResponsePtr Response, bool bConnectedSuccessfully);
+};
