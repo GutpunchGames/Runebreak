4db2feacd8b77ce36e53ea1da4478934ad6283b7
AndyG <anderson.garron@gmail.com>
2023-01-16 11:34:22 -0800

    made social list render data

--- runebreak-game/RunebreakGame/Source/RunebreakGame/Public/UI/SocialList.h
+++ runebreak-game/RunebreakGame/Source/RunebreakGame/Public/UI/SocialList.h
@@ -4,15 +4,45 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include <RunebreakGame/Public/RBGameSession/Types/CharacterClass.h>
 #include "SocialList.generated.h"
 
-UCLASS(BlueprintType)
-class RUNEBREAKGAME_API USocialListEntryData : public UObject {
+UENUM(BlueprintType)
+enum OnlineStatus
+{
+	Online	UMETA(DisplayName = "Online"),
+	Idle	UMETA(DisplayName = "Idle"),
+	Offline	UMETA(DisplayName = "Offline"),
+};
+
+USTRUCT(BlueprintType)
+struct RUNEBREAKGAME_API FSocialListEntryData {
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-	FString TextValue;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Username;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Subtext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TEnumAsByte<OnlineStatus> Status;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TEnumAsByte<CharacterClass> Class;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool ShouldShowClass = true;
+};
+
+UCLASS(BlueprintType)
+class RUNEBREAKGAME_API USocialListEntryDataContainer : public UObject {
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FSocialListEntryData Data;
 };
 
 /**
