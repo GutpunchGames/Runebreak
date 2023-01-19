e562830a19fea39829a3a3600354c76156cb69cb
AndyG <anderson.garron@gmail.com>
2023-01-18 17:28:41 -0800

    have lobbies joining and leaving working

--- runebreak-infra/lobbies/server/lobby_serializer.go
+++ runebreak-infra/lobbies/server/lobby_serializer.go
@@ -6,6 +6,33 @@ import (
 	lobbiesPbs "github.com/GutpunchGames/Runebreak/runebreak-infra/protos/lobbies"
 )
 
+type serializedLobby struct {
+	LobbyId   string  `json:"lobbyId"`
+	LobbyName string  `json:"lobbyName"`
+	OwnerId   string  `json:"ownerId"`
+	Users     []*serializedUser `json:"users"`
+}
+
+type serializedUser struct {
+	UserId   string `json:"userId"`
+	UserName string `json:"userName"`
+}
+
 func serializeLobby(lobby *lobbiesPbs.Lobby) ([]byte, error) {
-	return json.Marshal(lobby)
+	serializedUsers := make([]*serializedUser, 0)
+	for _, user := range lobby.Users {
+		serializedUsers = append(serializedUsers, &serializedUser{
+			UserId: user.UserId,
+			UserName: user.UserName,
+		})
+	}
+
+	serialized := serializedLobby{
+		LobbyId: lobby.LobbyId,
+		LobbyName: lobby.LobbyName,
+		OwnerId: lobby.OwnerId,
+		Users: serializedUsers,
+	}
+
+	return json.Marshal(serialized)
 }
