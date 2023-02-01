2e276bcc4255c978897be4bc66e6184c2704ebca
Brandon Cohen <cohbrandon@gmail.com>
2023-02-01 17:46:54 -0500

    deserialize moving ball

--- runebreak-game/RunebreakGame/Source/RunebreakGame/Private/GameOrchestrator/SaveState.cpp
+++ runebreak-game/RunebreakGame/Source/RunebreakGame/Private/GameOrchestrator/SaveState.cpp
@@ -2,9 +2,18 @@
 
 
 #include "GameOrchestrator/SaveState.h"
+#include "Misc/Base64.h"
 
 void USavedStateManager::Save(int Frame, TArray<ASimulationActor*> Actors) {
 	UE_LOG(LogTemp, Warning, TEXT("Saved state for frame: %d"), Frame)
+
+		FSavedSimulation SavedSimulation;
+		SavedSimulation.Frame = Frame;
+
+		for (ASimulationActor* SimulationActor : Array)
+		{
+			SimulationActor->WriteFunc()
+		}
 }
 
 FSavedSimulation USavedStateManager::GetSavedState(int Frame) {
--- runebreak-game/RunebreakGame/Source/RunebreakGame/Private/GameOrchestrator/SimulationMovingBall.cpp
+++ runebreak-game/RunebreakGame/Source/RunebreakGame/Private/GameOrchestrator/SimulationMovingBall.cpp
@@ -12,5 +12,23 @@ void ASimulationMovingBall::SimulationTick(int Frame, IInputBuffer* Player1Input
 }
 
 FString ASimulationMovingBall::SerializeState() {
-	return "";
+	FVector Location = GetActorLocation();
+	FString SerializedLocation = FString::Printf(TEXT("%d,%d,%d"), Location.X, Location.Y, Location.Z);
+	return SerializedLocation;
+}
+
+
+void ASimulationMovingBall::Deserialize(const FString& Bytes) {
+	FString X_STR;
+	FString Y_STR;
+	FString Z_STR;
+
+	Bytes.Split(TEXT(","), &X_STR, &Y_STR, &Z_STR);
+
+	FVector NewLocation;
+
+	NewLocation.X = FCString::Atof(*X_STR);
+	NewLocation.Y = FCString::Atof(*Y_STR);
+	NewLocation.Z = FCString::Atof(*Z_STR);
+	SetActorLocation(NewLocation);
 }
