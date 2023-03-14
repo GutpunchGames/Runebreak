d38a4805ca56fa83c7427ce7012a674a4dec4354
AndyG <anderson.garron@gmail.com>
2023-03-14 09:37:08 -0700

    added a unit test example

---
+++ runebreak-game/RunebreakGame/Source/RunebreakGame/Private/GameOrchestrator/Test/SerializationTest.h/SerializationTest.cpp
@@ -0,0 +1,13 @@
+#include "SerializationTest.h"
+
+bool FSerializationTestTwo::RunTest(FString const& Parameters) {
+	return false;
+}
+
+bool FSerializationTestThree::RunTest(FString const& Parameters) {
+	return true;
+}
+
+bool FSerializationTest4::RunTest(FString const& Parameters) {
+	return true;
+}
